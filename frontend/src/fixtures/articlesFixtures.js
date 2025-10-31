da6ea1cfa38936b1f75e0c71120f20293ca9effc
Charlie Nava <charlie220@ucsb.edu>
2025-10-31 01:48:57 -0700

    added team01 Articles files and fixtures for Articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,38 @@
+const articlesFixtures = {
+  oneArticles: {
+    id: 1,
+    title: "article1",
+    url: "howmuchmilk.com",
+    explanation: "people need to know",
+    email: "milkman@gmail.com"
+    dateAdded: "2022-01-02T12:00:00",
+  },
+  threeArticles: [
+    {
+      id: 1,
+      title: "article1",
+      url: "lactoseanonymous.com",
+      explanation: "its a problem and they are sturggling",
+      email: "milkman@gmail.com",
+      dateAdded: "2022-01-02T12:00:00",
+    },
+    {
+      id: 2,
+      title: "article2",
+      url: "article2.com",
+      explanation: "im too lazy to be creative",
+      email: "articles@gmail.com",
+      dateAdded: "2022-04-03T12:00:00",
+    },
+    {
+      id: 3,
+      title: "article3",
+      url: "something.com",
+      explanation: "something or something",
+      email: "something@gmail.com",
+      dateAdded: "2022-07-04T12:00:00",
+    },
+  ],
+};
+
+export { articlesFixtures };
