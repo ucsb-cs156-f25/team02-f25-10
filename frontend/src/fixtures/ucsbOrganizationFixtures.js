539f592db998ec8eff4edc745d801cb0c7f0376f
Saanvi Pabbichetty <spabbichetty@ucsb.edu>
2025-11-02 22:06:36 -0800

    added fixtures for UCSBOrganization

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,35 @@
+const ucsbOrganizationFixtures = {
+  oneOrganization: [
+    {
+      orgcode: "SWE",
+      orgTranslationShort: "SWE",
+      orgTranslation: "Society of Women Engineers",
+      inactive: false,
+    },
+  ],
+
+  threeOrganizations: [
+    {
+      orgcode: "SWE",
+      orgTranslationShort: "SWE",
+      orgTranslation: "Society of Women Engineers",
+      inactive: false,
+    },
+
+    {
+      orgcode: "WiCS",
+      orgTranslationShort: "WiCS",
+      orgTranslation: "Women in Computer Science",
+      inactive: false,
+    },
+    
+    {
+      orgcode: "ACM",
+      orgTranslationShort: "ACM",
+      orgTranslation: "Association for Computing Machinery",
+      inactive: true,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
