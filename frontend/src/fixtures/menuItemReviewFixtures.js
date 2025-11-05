63c4200e0daa7de6fb8e5415b610c9133db404b2
Keigo Omura <komura@ucsb.edu>
2025-11-04 16:17:25 -0800

    adding all Menu Item Review forms (form itself, in story, and tests). Also changing fixture formatting

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,38 +1,38 @@
 const menuItemReviewFixtures = {
-    oneMenuItemReview: {
-        id: 1,
-        itemId: 1,
-        reviewerEmail: "johndoe@ucsb.edu",
-        stars: 3,
-        dateReviewed: "2025-10-31T20:33:40.037Z",
-        comments: "Pretty good",
+  oneMenuItemReview: {
+    id: 1,
+    itemId: 1,
+    reviewerEmail: "johndoe@ucsb.edu",
+    stars: 3,
+    dateReviewed: "2025-10-31T20:33:40",
+    comments: "Pretty good",
+  },
+  threeMenuItemReviews: [
+    {
+      id: 1,
+      itemId: 1,
+      reviewerEmail: "johndoe@ucsb.edu",
+      stars: 3,
+      dateReviewed: "2025-10-31T20:33:40",
+      comments: "Pretty good",
     },
-    threeMenuItemReviews: [
-        {
-            id: 1,
-            itemId: 1,
-            reviewerEmail: "johndoe@ucsb.edu",
-            stars: 3,
-            dateReviewed: "2025-10-31T20:33:40.037Z",
-            comments: "Pretty good"
-        },
-        {
-            id: 2,
-            itemId: 2,
-            reviewerEmail: "kevin@ucsb.edu",
-            stars: 5,
-            dateReviewed: "2025-11-01T20:33:40.037Z",
-            comments: "Very yummy"
-        },
-        {
-            id: 3,
-            itemId: 2,
-            reviewerEmail: "michael@ucsb.edu",
-            stars: 1,
-            dateReviewed: "2025-11-02T20:33:40.037Z",
-            comments: "Bad"
-        },
-    ],
+    {
+      id: 2,
+      itemId: 2,
+      reviewerEmail: "kevin@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2025-11-01T20:33:40",
+      comments: "Very yummy",
+    },
+    {
+      id: 3,
+      itemId: 2,
+      reviewerEmail: "michael@ucsb.edu",
+      stars: 1,
+      dateReviewed: "2025-11-02T20:33:40",
+      comments: "Bad",
+    },
+  ],
 };
 
-export{menuItemReviewFixtures};
+export { menuItemReviewFixtures };
