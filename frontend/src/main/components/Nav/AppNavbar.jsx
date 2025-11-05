15cb2c0a012ea4a07ec4e73edbac856f1b9542f2
Sarahbob <71357251+Sarahbob@users.noreply.github.com>
2025-11-05 01:21:15 -0800

    Update AppNavbar.jsx

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -72,7 +72,7 @@ export default function AppNavbar({
                     Recommendation Request
                   </Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
-                    UCSBDiningCommonsMenuItem
+                    UCSB Dining Commons Menu Item
                   </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
