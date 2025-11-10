701fb61a9f4a481c8ec716e989deffbd903b1b6e
Charlie Nava <charlie220@ucsb.edu>
2025-11-09 23:38:24 -0800

    readded placeholders for index and edit

---
+++ frontend/src/main/pages/Articles/ArticlesEditPage.jsx
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+
+// Stryker disable all
+export default function ArticlesEditPage() {
+  // placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Edit page not yet implemented</h1>
+      </div>
+    </BasicLayout>
+  );
+}
---
+++ frontend/src/main/pages/Articles/ArticlesIndexPage.jsx
@@ -0,0 +1,20 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+
+// Stryker disable all
+export default function ArticlesIndexPage() {
+  // placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Index page not yet implemented</h1>
+        <p>
+          <a href="/articles/create">Create</a>
+        </p>
+        <p>
+          <a href="/articles/edit/1">Edit</a>
+        </p>
+      </div>
+    </BasicLayout>
+  );
+}
